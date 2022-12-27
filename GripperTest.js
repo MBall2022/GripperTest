ba36d1c1a7286b1036f940931e1a9272a5493a63
MBall2022 <120830333+MBall2022@users.noreply.github.com>
2022-12-26 19:51:02 -0500

    Update GripperTest.js

--- GripperTest.js
+++ GripperTest.js
@@ -207,7 +207,7 @@ scene.add( light3 );
 var loader = new THREE.TextureLoader();
 loader.setCrossOrigin("anonymous");
 const texture = loader.load(
-  'pics/tileFloor.jpeg');
+  'assets/tileFloor.jpeg');
 
   var Fmaterial = new THREE.MeshPhongMaterial({ color:0xFFFFFF, map: texture });
   texture.wrapS = THREE.RepeatWrapping;
